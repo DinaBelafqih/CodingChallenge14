9df5305b2ef17bcaea09bee530ebfb23bb022b3f
dinabelafqih <belafqihdina@gmail.com>
2025-03-09 21:23:30 -0400

    Task 5 - Inline Editing for Support Tickets

--- cc_14.js
+++ cc_14.js
@@ -36,3 +36,47 @@ function addTicket(customerName, issueDescription, priorityLevel) {
     ticket.appendChild(priorityState);
     ticket.appendChild(resolveButton);
     ticketContainer.appendChild(ticket); // appending ticket to container
+
+    // Task 5 - Inline Editing for Support Tickets
+    ticket.addEventListener('dblclick', function() {
+        const nameInput = document.createElement('input');
+        nameInput.setAttribute('type', 'text');
+        nameInput.value = nameHeading.textContent;
+
+        const issueInput = document.createElement('input');
+        issueInput.setAttribute('type', 'text');
+        issueInput.value = issueText.textContent;
+
+        const priorityInput = document.createElement('input');
+        priorityInput.setAttribute('type', 'text');
+        priorityInput.value = priorityState.textContent.split(": ")[1]; // trying to get current priority
+
+        // making save button
+        const saveButton = document.createElement('button');
+        saveButton.textContent = 'Save';
+
+        // clearing current content
+        ticket.innerHTML = '';
+
+        // appending again just in case
+        ticket.appendChild(nameInput);
+        ticket.appendChild(issueInput);
+        ticket.appendChild(priorityInput);
+        ticket.appendChild(saveButton);
+
+        // save button listener
+        saveButton.addEventListener('click', function() {
+            // updating new values
+            nameHeading.textContent = nameInput.value;
+            issueText.textContent = issueInput.value;
+            priorityState.textContent = `Priority: ${priorityInput.value}`;
+            priorityState.setAttribute('class', priorityInput.value.toLowerCase());
+
+            // reverting back fields to static text
+            ticket.innerHTML = '';
+            ticket.appendChild(nameHeading);
+            ticket.appendChild(issueText);
+            ticket.appendChild(priorityState);
+            ticket.appendChild(resolveButton);
+        });
+    });
